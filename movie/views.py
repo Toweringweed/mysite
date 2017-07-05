21239ac113518eeff7f495bb126994689425867a
Toweringweed <xiaohou09@gmail.com>
2017-07-05 17:22:05 +0800

    g
    g

--- movie/views.py
+++ movie/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Mdetail
 
 # Create your views here.
 def index(request):
-    return HttpResponse("<h1>Hello!</h1>")
+    last_list = Mdetail.objects.order_by('m_update_douban')[:5]
+    output = ','.join([p.m_name for p in last_list])
+    return HttpResponse(output)
 
